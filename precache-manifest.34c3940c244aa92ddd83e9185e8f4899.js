ccec0befd2bb8351d25719ea883f6aca62758f48
Camilo Aljoscha Kosniowski Marquez <kosniowski@MacBook-Pro-de-Camilo.local>
2018-10-29 21:47:13 -0500

    Updates

---
+++ precache-manifest.34c3940c244aa92ddd83e9185e8f4899.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44fbb17175c5ab19beb0",
+    "url": "/ejercicio.io/static/js/runtime~main.44fbb171.js"
+  },
+  {
+    "revision": "bfaf473d545417cff1d9",
+    "url": "/ejercicio.io/static/js/main.bfaf473d.chunk.js"
+  },
+  {
+    "revision": "679b7e1abdd4da71fdf7",
+    "url": "/ejercicio.io/static/js/1.679b7e1a.chunk.js"
+  },
+  {
+    "revision": "bfaf473d545417cff1d9",
+    "url": "/ejercicio.io/static/css/main.aef2106b.chunk.css"
+  },
+  {
+    "revision": "893e8302eca1439521adae891e61ed06",
+    "url": "/ejercicio.io/index.html"
+  }
+];
